270ceab65235689efac99e521176266d261be010
AngeloModanez <angelomodanez15@gmail.com>
2025-04-10 11:54:21 -0300

    Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,82 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from './components/Container';
+import './container.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [entries, setEntries] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const newEntry = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      telefone: formData.get("telefone"),
+      senha: formData.get("senha"),
+    }
+
+    const found = entries.find(entry => entry.email === newEntry.email);
+    if (found) {
+      alert("Email já cadastrado " + found.email);
+      return;
+    }
+
+    setEntries((prev) => [...prev, newEntry]);
+    e.target.reset();
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className="container">
+        <Container className="sub">
+          <div className="header">
+            <h1>Cadastro de Aluno</h1>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nome</label>
+            <input type="text" name='name' id='name' required />
+
+            <label htmlFor="email">Email</label>
+            <input type="text" name='email' id='email' required />
+
+            <label htmlFor="telefone">Telefone</label>
+            <input type="text" name='telefone' id='telefone' required />
+
+            <label htmlFor="senha">Senha</label>
+            <input type="text" name='senha' id='senha' required />
+
+            <button type='submit' className='submit-btn'>Submit</button>
+          </form>
+        </Container>
+
+        <Container className='sub'>
+          {entries.length > 0 && (
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Telefone</th>
+                  <th>Senha</th>
+                </tr>
+              </thead>
+              <tbody>
+                {entries.map((entry, index) => (
+                  <tr key={index}>
+                    <td>{entry.name}</td>
+                    <td>{entry.email}</td>
+                    <td>{entry.telefone}</td>
+                    <td>{entry.senha}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </Container>
+      </Container>
     </>
   )
 }
